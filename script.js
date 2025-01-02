5660bc28ce396146e9a4ab7183124cfb2e0f655b
Sch0udh4ry <rebelsbharat@gmail.com>
2025-01-02 22:50:18 +0530

    added CSS/JS

---
+++ script.js
@@ -0,0 +1,32 @@
+// Smooth scrolling
+document.querySelectorAll('a.nav-link').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Active menu highlight
+const sections = document.querySelectorAll('section');
+const navLi = document.querySelectorAll('nav .nav-link');
+
+window.addEventListener('scroll', () => {
+    let current = '';
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+        if (pageYOffset >= (sectionTop - sectionHeight / 3)) {
+            current = section.getAttribute('id');
+        }
+    });
+
+    navLi.forEach(li => {
+        li.classList.remove('active');
+        if (li.getAttribute('href').includes(current)) {
+            li.classList.add('active');
+        }
+    });
+});
