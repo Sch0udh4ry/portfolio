d96f66f4ea56018a32abe14b119faef6fff38b55
LISUN haryOUDGB <rebelsbharat@gmail.com>
2026-03-29 20:11:21 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,12 @@
 import "./globals.css";
 import type { ReactNode } from "react";
+import Navbar from "@/components/layout/Navbar"; // 
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body>
+        <Navbar /> {/*  THIS */}
         {children}
       </body>
     </html>
